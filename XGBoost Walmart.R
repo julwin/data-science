68e3f50e8db6a6ed7dcdb30c83fa5564a034ed8c
Julius Winkelmann <julwin@users.noreply.github.com>
2016-04-05 21:46:07 +0200

    XGBoost for Walmart Kaggle competition

---
+++ XGBoost Walmart.R
@@ -0,0 +1,49 @@
+require(xgboost)
+require(methods)
+require(reshape2)
+
+featureclass = rep('numeric', length(classnames))
+#colclasstrain = c('integer', featureclass, 'character')
+#colclasstest = c('integer', featureclass)
+
+train = read.csv('train_cleaned.csv', header=T)#, colClasses=colclasstrain)
+test = read.csv('test_cleaned.csv', header=T)#, colClasses = colclasstest)
+
+
+#id = test[,1]
+
+#train = train[,-1]
+#test = test[,-1]
+
+target = length(colnames(train))-1
+classnames = unique(target)
+target = as.integer(classnames)
+
+#train = train[,-ncol(train)]
+
+trainMatrix <- data.matrix(train)
+testMatrix <- data.matrix(test)
+
+numberOfClasses <-max(target) +1
+
+param <- list('objective' = 'multi:softprob', 'eval_metric' = 'mlogloss', 'num_class' = numberOfClasses)
+
+cv.nround <- 500
+cv.nfold <- 5
+bst.cv = xgb.cv(param=param, data = trainMatrix, label = train$TripType, nfold = cv.nfold, nrounds = cv.nround)
+
+plot(bst.cv$test.mlogloss.mean, lty = '1')
+nround <- which(bst.cv$test.mlogloss.mean==min(bst.cv$test.mlogloss.mean))
+
+bst = xgboost(data = trainMatrix, label = target, params = param, nrounds = nround)
+
+
+ypred = predict(bst, testMatrix)
+
+predMatrix <- data.frame(matrix(ypred, ncol=numberOfClasses, byrow=TRUE))
+colnames(predMatrix) = classnames
+res <- data.frame(id, predMatrix)
+write.csv(res, 'submissionR.csv', quote=F, row.names=F)
+
+
+
